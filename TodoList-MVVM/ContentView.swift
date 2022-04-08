f98b5ca4c6fdefa33553660d6f74eb2522871c8c
abdWasiq <abdul.wasiq@naheed.pk>
2022-04-08 20:36:40 +0500

    project setup

---
+++ TodoList-MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList-MVVM
+//
+//  Created by NaheedPK on 08/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList-MVVM/TodoList_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_MVVMApp.swift
+//  TodoList-MVVM
+//
+//  Created by NaheedPK on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
