9452f2a1a556dde61de12be99f42cf0c9b6dc66f
abdWasiq <abdul.wasiq@naheed.pk>
2022-05-10 12:26:40 +0500

    setup firebase

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TodoList-MVVM' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TodoList-MVVM
+pod 'Firebase/Auth'
+pod 'Firebase/Core'
+
+end
--- TodoList-MVVM/TodoList_MVVMApp.swift
+++ TodoList-MVVM/TodoList_MVVMApp.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
-
+import Firebase
 @main
 struct TodoList_MVVMApp: App {
     @StateObject var listViewModel: ListViewModel = ListViewModel()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             NavigationView {
@@ -17,5 +18,14 @@ struct TodoList_MVVMApp: App {
             }
             .environmentObject(listViewModel)
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
     }
 }
